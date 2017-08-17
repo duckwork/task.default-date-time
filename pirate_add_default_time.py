1e93e00f7e918a797360225f3d3f32d860c75fe8
Case Duckworth <cased123@gmail.com>
2017-08-16 19:50:56 -0500

    Use taskrc

--- pirate_add_default_time.py
+++ pirate_add_default_time.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python
 from __future__ import print_function
-from datetime import time
-from tasklib import Task, local_zone
+from datetime import time, datetime, timedelta
+from tasklib import Task, local_zone, TaskWarrior
+from pytz import timezone, utc
 
-DEFAULT_TIME = time(22,0,0)  # Your wanted default time
+# DEFAULT_TIME = time(22,0,0)  # Your wanted default time
+conf = TaskWarrior().config
+
+def get_conf_value(config, value, default, process=(lambda x: x)):
+    if value in config:
+        if isinstance(default, bool):
+            if config[value].lower() in ['true', '1', 'yes']:
+                return True
+            else: return False
+        else: return process(config[value])
+    else: return default
+
+default_time = get_conf_value(conf, 'default.time', 
+        time(0,0,0), 
+        (lambda t: datetime.strptime(t, u'%H:%M').time()))
 
 def is_local_midnight(timestamp):
     return timestamp.astimezone(local_zone).time() == time(0,0,0)
 
 def set_default_time(timestamp):
-    return timestamp.astimezone(local_zone).replace(
-        hour=DEFAULT_TIME.hour,
-        minute=DEFAULT_TIME.minute,
-        second=DEFAULT_TIME.second,
+    return timestampastimezone(local_zone).replace(
+        hour=default_time.hour,
+        minute=default_time.minute,
+        second=default_time.second,
         )
 
 def hook_default_time(task):
     if task['due'] and is_local_midnight(task['due']):
         task['due'] = set_default_time(task['due'])
         print("Default due time has been set.")
+
+if __name__ == '__main__':
+    print(default_time)
